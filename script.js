3e8e582469c787fb2b4d760ee769a948b16c2f46
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-01-28 23:42:55 +0000

    Targeting html elements by id

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Targeting html elements by id and storing API key
+const search = document.getElementById("searchUser");
+const button = document.getElementById("submit");
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=988fbbe10b9a8419e74f5e6d95338e7c"
 )
